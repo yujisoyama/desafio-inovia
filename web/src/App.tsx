2c72addf8b38b132a96e76bd53a0faa1ed8c6e9b
Fabio Yuji Soyama <yuji.soyama@gmail.com>
2022-12-09 10:51:10 -0300

    Add web initial files

---
+++ web/src/App.tsx
@@ -0,0 +1,10 @@
+import './styles/main.css'
+
+function App() {
+  return (
+    <div className='bg-slate-500'>
+      Hello World
+    </div>
+  )
+}
+export default App
---
+++ web/tailwind.config.cjs
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: [
+    './src/**/*.tsx',
+    './index.html'
+  ],
+  theme: {
+    extend: {
+      colors: {
+
+      },
+    },
+    screens: {
+
+    },
+    fontFamily: {
+
+    },
+    extend: {
+      backgroundImage: {
+
+      },
+    },
+  },
+  plugins: [
+
+  ],
+}
+

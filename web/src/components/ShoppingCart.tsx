77d14f2ba449903840b33ac41a4d7ee40b8155e6
Fabio Yuji Soyama <yuji.soyama@gmail.com>
2022-12-16 23:21:31 -0300

    Add number to ShoppingCart

--- web/src/components/ShoppingCart.tsx
+++ web/src/components/ShoppingCart.tsx
@@ -2,7 +2,8 @@ import { ShoppingCartSimple } from 'phosphor-react'
 
 export const ShoppingCart = () => {
     return (
-        <div className='p-2 rounded-lg border border-background hover:cursor-pointer hover:border-highlight duration-100'>
+        <div className='p-2 rounded-lg border border-background hover:cursor-pointer hover:border-highlight duration-100 flex items-center gap-2 text-highlight font-noto font-extrabold'>
+            <p>12</p>
             <ShoppingCartSimple size={40} color="#1de9b6" weight="bold" />
         </div>
     )
